f5d7206b1584a1239901883737d2423691326f26
Bruno Maldonado <79159400+brunomaldonado@users.noreply.github.com>
2021-03-01 00:41:23 -0600

    Layout Mobile
    Challenge

---
+++ demo/demo.js
@@ -0,0 +1,5 @@
+// alert("hello wolrd")
+document.querySelector('.btn-menu').addEventListener('click', ()=>{
+  // console.log('clik!!!');
+  document.querySelector('.nav-items').classList.toggle('show');
+});
